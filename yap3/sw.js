7748630ec14cfb8d288ba637743d43c1fa117940
Maikol <maikolecrisanto@gmail.com>
2026-02-20 20:26:38 -0500

    YPTONV1

--- yap3/sw.js
+++ yap3/sw.js
@@ -1,11 +1,60 @@
 // sw.js - Service Worker Nivel Producción (Anti-fallos)
-const CACHE_NAME = 'yape-v2'; // Cambiar este número fuerza la actualización en los celulares
+const CACHE_NAME = 'yape-v4'; // Cambiado a v4 para forzar la descarga de la bóveda total
 
-// Solo cacheamos lo ultra necesario para que la instalación nunca falle
+// Lista VIP de memoria: cacheamos todo el núcleo de la app para que cargue en 0 segundos
 const urlsToCache = [
   '/',
   '/index.html',
-  '/img/favicon.png'
+  '/inicio.html',
+  '/login_pin.html',
+  '/escanear.html',
+  '/yapear.html',
+  '/monto.html',
+  '/exito.html',
+  '/exito_editable.html',
+  '/exito_servicios.html',
+  '/servicios.html',
+  '/perfil.html',
+  '/opciones.html',
+  '/sobre.html',
+  '/style.css',
+  '/seguridad.js',
+  '/manifest.json',
+  '/img/favicon.png',
+  '/img/animationyape.gif',
+  '/img/campana-icon.svg',
+  '/img/qr-icon.svg',
+  '/img/icono_yapear.png',
+  '/img/icono_movimientos.png',
+  '/img/iconoperfil.png',
+  '/img/logo_yape_header.png',
+  '/img/homeanim1.gif',
+  '/img/loginanim1.gif',
+  '/img/check_exito.png',
+  '/img/codigo-seguridad.png',
+  '/img/compartiricon.png',
+  '/img/fechaicon.png',
+  '/img/hora-icon.png',
+  '/img/mensaje-icon1.png',
+  '/img/ojo_abierto.png',
+  '/img/ojo_cerrado.png',
+  '/img/qr_login.png',
+  '/img/flecha-icon.svg',
+  '/img/descarga.gif',
+  '/img/download.png',
+  '/img/download1.png',
+  '/img/download2.png',
+  '/img/download3.png',
+  '/img/download4.png',
+  '/img/download5.png',
+  '/img/download6.png',
+  '/img/download7.png',
+  '/img/download8.png',
+  '/img/download9.png',
+  '/img/download10.png',
+  '/img/download11.png',
+  '/img/download13.png',
+  '/img/download14.png'
 ];
 
 // 1. INSTALACIÓN (Sin morir en el intento)
